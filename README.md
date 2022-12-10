6f133ae8707bf416c7e643dba0b0be8fb231f27f
Sergey Radyukhin <22dollars@mail.ru>
2022-12-10 11:11:56 +0300

    add BootStrap

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[domain](https://python-project-83-production-a375.up.railway.app/)
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/sraduhin/python-project-83/workflows/hexlet-check/badge.svg)](https://github.com/sraduhin/python-project-83/actions)
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'project 83'
+    project_name = 'project 83'
+    return render_template('index.html', project_name=project_name)
 
 if __name__ == '__main__':
     app.run()
